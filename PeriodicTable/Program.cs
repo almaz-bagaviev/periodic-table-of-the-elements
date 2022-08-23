0791311e056763d31111ea47c4b97b5592e1fddc
almaz-bagaviev <almazbagaviev22@gmail.com>
2022-08-23 18:20:06 +0300

    Add void ConfigureServices() & void Configure() in Startup

--- PeriodicTable/Program.cs
+++ PeriodicTable/Program.cs
@@ -1,25 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-/*public void ConfigureServices(IServiceCollection services)*/
-builder.Services.AddRazorPages();
-builder.Services.AddMvc();
+var startup = new Startup(builder.Configuration);
 
-/*public void Configure(IApplicationBuilder app)*/
+startup.ConfigureServices(builder.Services);
+
 var app = builder.Build();
-//app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting(); // Необходимо использовать для endpoints.MapControllerRoute
-
-//app.UseAuthorization();
-//app.MapRazorPages();
-
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-    name: "Default",
-    pattern: "{Controller=Home}/{Action=Index}");
-});
-
+startup.Configure(app, app.Environment);
 app.Run();
---
+++ PeriodicTable/Startup.cs
@@ -0,0 +1,33 @@
+﻿namespace PeriodicTable;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+    
+    public Startup(IConfiguration configuration)
+    {
+        this.Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddRazorPages();
+        services.AddMvc();
+
+        services.AddDbContext<ChemicalElementsContext>(options =>
+                 options.UseSqlServer(Configuration.GetConnectionString("ChemicalElementsConnection")));
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseStaticFiles();
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+            name: "Default",
+            pattern: "{Controller=Home}/{Action=Index}");
+        });
+    }
+}
