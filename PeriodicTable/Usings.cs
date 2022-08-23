d05f56d5b33148f3812de7e1387f3762d4051844
almaz-bagaviev <almazbagaviev22@gmail.com>
2022-08-23 18:38:59 +0300

    add usings EF Core and Contexts

--- PeriodicTable/Usings.cs
+++ PeriodicTable/Usings.cs
@@ -1,4 +1,8 @@
 ﻿global using PeriodicTable.Controllers;
+global using PeriodicTable;
 global using PeriodicTable.Entities;
 global using PeriodicTable.Models;
 global using PeriodicTable.Web;
+global using PeriodicTable.Contexts;
+global using Microsoft.EntityFrameworkCore;
+global using System.ComponentModel.DataAnnotations.Schema; //Type name of Columns
