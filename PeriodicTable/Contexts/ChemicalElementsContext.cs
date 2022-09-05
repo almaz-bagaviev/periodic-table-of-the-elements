c84df86e1bccf10ecf57ad34086090f9cbb64a63
almaz-bagaviev <almazbagaviev22@gmail.com>
2022-09-05 18:35:56 +0300

    class : IdentityDbContext<ApplicationUser>

--- PeriodicTable/Contexts/ChemicalElementsContext.cs
+++ PeriodicTable/Contexts/ChemicalElementsContext.cs
@@ -1,13 +1,13 @@
 ﻿namespace PeriodicTable.Contexts;
 
-public class ChemicalElementsContext : DbContext
+public class ChemicalElementsContext : IdentityDbContext<ApplicationUser>
 {
     public DbSet<ChemicalElement> ChemicalElementsTable { get; set; }
     public DbSet<Lanthanide> LanthanidesTable { get; set; }
     public DbSet<Actinide> ActinidesTable { get; set; }
     public DbSet<Link> LinksTable { get; set; }
 
-    public ChemicalElementsContext(DbContextOptions<ChemicalElementsContext> options)
+    public ChemicalElementsContext(DbContextOptions options)
         : base(options)
     {
 
