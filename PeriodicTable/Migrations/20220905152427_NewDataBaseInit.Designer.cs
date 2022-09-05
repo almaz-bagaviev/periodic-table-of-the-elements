488dd7d6ce425651738917ff2bd8343994812a07
almaz-bagaviev <almazbagaviev22@gmail.com>
2022-09-05 18:42:23 +0300

    New DB

--- PeriodicTable/Migrations/20220905152427_NewDataBaseInit.Designer.cs
+++ PeriodicTable/Migrations/20220905152427_NewDataBaseInit.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PeriodicTable.Contexts;
 
@@ -11,9 +12,10 @@ using PeriodicTable.Contexts;
 namespace PeriodicTable.Migrations
 {
     [DbContext(typeof(ChemicalElementsContext))]
-    partial class ChemicalElementsContextModelSnapshot : ModelSnapshot
+    [Migration("20220905152427_NewDataBaseInit")]
+    partial class NewDataBaseInit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
