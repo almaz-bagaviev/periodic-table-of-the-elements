cb9417a1b1675814632af4465128b9537501b7a3
almaz-bagaviev <almazbagaviev22@gmail.com>
2022-09-05 18:45:51 +0300

    usings for Authentication

--- PeriodicTable/Usings.cs
+++ PeriodicTable/Usings.cs
@@ -4,5 +4,12 @@ global using PeriodicTable.Entities;
 global using PeriodicTable.Models;
 global using PeriodicTable.Web;
 global using PeriodicTable.Contexts;
+global using Microsoft.AspNetCore.Identity;
 global using Microsoft.EntityFrameworkCore;
 global using System.ComponentModel.DataAnnotations.Schema; //Type name of Columns
+global using PeriodicTable.Account;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using System.ComponentModel.DataAnnotations;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Authentication; //for AccountController
+global using Microsoft.AspNetCore.Authentication.Cookies; //for AccountController
