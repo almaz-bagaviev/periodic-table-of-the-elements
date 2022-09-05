83f6b6225e1dc557793526b623063318d8931bd1
almaz-bagaviev <almazbagaviev22@gmail.com>
2022-09-05 18:50:22 +0300

    add LogoutViewComponent

---
+++ PeriodicTable/Component/LogoutViewComponent.cs
@@ -0,0 +1,11 @@
+﻿namespace WebClients.Component;
+
+public class LogoutViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+
+    //Примечание. Имя метода должен быть именно Invoke(), не Default
+}
