5889b5068d115e0b23a5e3981b56f726fb90a34b
almaz-bagaviev <almazbagaviev22@gmail.com>
2022-09-05 18:44:37 +0300

    add Configures for Authorization

--- PeriodicTable/Startup.cs
+++ PeriodicTable/Startup.cs
@@ -12,10 +12,34 @@ public class Startup
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddRazorPages();
+        //services.AddTransient<IElement, ChemicalElement>();
         services.AddMvc();
 
         services.AddDbContext<ChemicalElementsContext>(options =>
                  options.UseSqlServer(Configuration.GetConnectionString("ChemicalElementsConnection")));
+
+        services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<ChemicalElementsContext>()
+            .AddDefaultTokenProviders();
+
+        services.Configure<IdentityOptions>(options =>
+        {
+            options.Password.RequiredLength = 6; //минимальное кол-во знаков в пароле
+            options.Lockout.MaxFailedAccessAttempts = 5; //кол-во попыток, после чего => блокировка
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+            options.Lockout.AllowedForNewUsers = true;
+        });
+
+        services.ConfigureApplicationCookie(options =>
+        {
+            // конфигурация Cookie с целью использования их для хранения авторизации
+            options.Cookie.HttpOnly = true;
+            options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+            options.LoginPath = "/Account/Login";
+            options.LogoutPath = "/Account/Logout";
+            options.SlidingExpiration = true;
+        });
+
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -27,7 +51,7 @@ public class Startup
         {
             endpoints.MapControllerRoute(
             name: "Default",
-            pattern: "{Controller=Home}/{Action=Index}");
+            pattern: "{Controller=Home}/{Action=Start}");
         });
     }
 }
