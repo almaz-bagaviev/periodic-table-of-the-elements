45744f229dad16b4c5cf244b254bde398d413e91
almaz-bagaviev <almazbagaviev22@gmail.com>
2022-10-24 17:13:38 +0300

    update db

---
+++ PeriodicTable/Migrations/20221024140141_AtomicNumberIsDouble.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PeriodicTable.Migrations
+{
+    public partial class AtomicNumberIsDouble : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "AverageAtomicMass",
+                table: "LanthanidesTable",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(8)");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "AverageAtomicMass",
+                table: "ChemicalElementsTable",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(8)");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "AverageAtomicMass",
+                table: "ActinidesTable",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(8)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AverageAtomicMass",
+                table: "LanthanidesTable",
+                type: "nvarchar(8)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AverageAtomicMass",
+                table: "ChemicalElementsTable",
+                type: "nvarchar(8)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AverageAtomicMass",
+                table: "ActinidesTable",
+                type: "nvarchar(8)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
